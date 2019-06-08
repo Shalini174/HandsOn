bc2c90ed3578795935c171fcc52a242641beda96
Shalini174 <39622811+Shalini174@users.noreply.github.com>
2019-06-08 19:51:35 +0530

    Update Step1.groovy

--- vars/Step1.groovy
+++ vars/Step1.groovy
@@ -1,3 +1,10 @@
-def Example() {
-      println('Hello World');
-   }
+class Example {
+   static def DisplayName() {
+      println("This is how methods work in groovy");
+      println("This is an example of a simple method");
+   } 
+	
+   static void main(String[] args) {
+      DisplayName();
+   } 
+}
